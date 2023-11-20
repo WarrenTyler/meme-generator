e89a52de31dff314136f7461cd7f388b7999be6c
WarrenT <warrentyler1024@gmail.com>
2023-11-20 21:56:13 +0000

    Add custom color and gridTemplateRows to tailwind.config
    - Introduce a custom color named 'grayish' with the hex code '#D5D4D8'.
    - Define a custom gridTemplateRows named 'form-layout' with row heights of '40px 40px'.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        grayish: "#D5D4D8",
+      },
       fontFamily: {
         karla: ["Karla", "sans-serif"],
       },
@@ -10,6 +13,9 @@ export default {
         "gradient-purple":
           "linear-gradient(90deg, #672280 1.18%, #A626D3 100%)",
       }),
+      gridTemplateRows: {
+        "form-layout": "40px 40px",
+      },
     },
   },
   plugins: [],
