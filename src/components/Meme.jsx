e020c3080bc433935c47a6aa6297c7c882fa69d5
WarrenT <warrentyler1024@gmail.com>
2023-11-22 18:10:02 +0000

    Add Meme component for dynamic meme generation
    - Create a new Meme component that allows users to input top and bottom text.
    - Fetch meme images from the provided data and display a random image.
    - Implement functionality to update the meme image and text dynamically.
    - Style the Meme component with Tailwind CSS for a visually appealing interface.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,21 +1,71 @@
+import { useState } from "react";
+import memesData from "../memesData";
+
 const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  const getMemeImage = (event) => {
+    event.preventDefault();
+
+    const { memes } = allMemeImages.data;
+    const { url } = memes[Math.floor(Math.random() * memes.length)];
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
   return (
     <main className="p-9">
       <form className="grid grid-rows-form-layout grid-cols-2 gap-4">
         <input
+          name="topText"
           className="border rounded-md border-grayish pl-2"
           type="text"
           placeholder="Top text"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
+          name="bottomText"
           className="border rounded-md border-grayish pl-2"
           type="text"
           placeholder="Bottom text"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
-        <button className="col-span-full text-white bg-gradient-purple">
+        <button
+          className="col-span-full text-white bg-gradient-purple"
+          onClick={getMemeImage}
+        >
           Get a new meme image 🖼
         </button>
       </form>
+      <div className="relative pt-4">
+        <img src={meme.randomImage} className="w-full rounded-sm" />
+        <h2 className="absolute w-80 text-center left-1/2 transform -translate-x-1/2 my-4 px-2 font-impact text-2xl uppercase text-white tracking-wide top-0 text-shadow shadow-black">
+          {meme.topText}
+        </h2>
+        <h2 className="absolute w-80 text-center left-1/2 transform -translate-x-1/2 my-4 px-2 font-impact text-2xl uppercase text-white tracking-wide bottom-0 text-shadow shadow-black">
+          {meme.bottomText}
+        </h2>
+      </div>
     </main>
   );
 };
